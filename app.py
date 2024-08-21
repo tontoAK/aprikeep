b01ba4907acdc47850fe40b65f1fff95cde01b1e
tontoAK <tonto17@nifty.com>
2024-08-22 06:35:10 +0900

    ver6 modified

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 from PIL import Image
 
 st.title("口コミ評判アプリ")
-st.write("""### サイドバーより、ポジネガ分析を選択して下さい""")
+st.write("""### サイドバーより分析方法を選択して下さい""")
 image = Image.open('AI6.jpg')
 st.image(image, caption='自然言語処理',width=300)
