2b09dd51eac273bc4674e390aeff17cdf039c266
tontoAK <tonto17@nifty.com>
2024-10-14 15:22:09 +0900

    GITにファイル修正

--- pages/5_WordCloud_English.py
+++ pages/5_WordCloud_English.py
@@ -15,17 +15,17 @@ if uploaded_file is not None:
   #data= pd.read_excel(uploaded_file)
   tg_col = st.selectbox('対象列選択,テキスト以外または容量制限でエラーになります',data.columns)
     
-    if st.button('実行'):
-      input_text = data[tg_col]
+  if st.button('実行'):
+    input_text = data[tg_col]
       
-      tw_words = ' '.join(input_text)
-      tw_wc = WordCloud()
-      tw_wc.generate(tw_words)
-      plt.figure(figsize=(10, 5))
-      plt.imshow(tw_wc)
-      plt.axis("off")
+    tw_words = ' '.join(input_text)
+    tw_wc = WordCloud()
+    tw_wc.generate(tw_words)
+    plt.figure(figsize=(10, 5))
+    plt.imshow(tw_wc)
+    plt.axis("off")
       
 
-      # Output
-      st.write(f""" #### word cloud""")
-      st.pyplot(plt)
+    # Output
+    st.write(f""" #### word cloud""")
+    st.pyplot(plt)
